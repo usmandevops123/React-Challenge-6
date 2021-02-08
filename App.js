5dd0579583dc083403d0a032aec12915b9de0ed6
Muhammad Usman <61974138+usmandevops123@users.noreply.github.com>
2021-02-08 13:20:05 -0800

    upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react'; 
+import SlotM from './Slotmachine';
+const App = () => {
+return( 
+  <>
+  <h1 className="heading_style">
+  🎰 Welcome to {" "}
+  <span style={{fontWeight :'bold'}}> Slot machine game </span> 🎰</h1>
+   
+     <div className="slotmachine">
+     <SlotM x='😄' y='😄' z='😄' /> 
+     <hr />
+     <SlotM x='😄' y=' 👍' z=' 🥇' /> 
+     <hr />
+     <SlotM x='🥰' y=' 🎅' z=' 🍶' /> 
+     <hr />
+     <SlotM x='😄' y='😄' z='😄' /> 
+     <hr />
+     
+     
+     
+     </div>
+   
+   </> 
+);
+
+};
+
+
+export default App;
